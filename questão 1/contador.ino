a0cf224b0d98593ff1eee8b1069cff1159c7e75b
KemilyRezende <102739889+KemilyRezende@users.noreply.github.com>
2024-04-18 15:08:50 -0300

    Update contador.ino

--- questão 1/contador.ino
+++ questão 1/contador.ino
@@ -3,9 +3,7 @@ volatile bool buttonPressed = false;
 unsigned long last_interrupt_time = 0;
 
 void press() {
-  // Marca o tempo da interrupção atual
   unsigned long interrupt_time = millis();
-  // Se a última interrupção ocorreu a menos de 400ms considera como bounce
   if (interrupt_time - last_interrupt_time > 450) 
   {
     buttonPressed = true;
@@ -15,12 +13,12 @@ void press() {
 
 void setup() {
   Serial.begin(9600);
-  pinMode(2, INPUT_PULLUP); // Configura o pino 2 como entrada com pull-up interno
-  attachInterrupt(digitalPinToInterrupt(2), press, FALLING); // Muda para FALLING
+  pinMode(2, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(2), press, FALLING);
 }
 
 void loop() {
-  if(buttonPressed){ //Ao pressionar o botão conta a interrupção
+  if(buttonPressed){ 
     counter++;
     Serial.print("Contador: ");
     Serial.println(counter);
